a7378157ab42600e29f5acfcfd7d80b3201536c8
Erick <154282688+ERICK13243@users.noreply.github.com>
2024-04-06 14:18:30 +0100

    info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -4,15 +4,15 @@ const handler = async (m, {conn, usedPrefix}) => {
   const text = `*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂  - 𝚃𝙴𝙰𝙼 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
 
 *➤ 𝙶𝚛𝚞𝚙𝚘𝚜 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎𝚕 𝙱𝚘𝚝:*
-*1.-* https://chat.whatsapp.com/Bi2Oql7vCjVJf2sFAZOzNi
+*1.-* https://chat.whatsapp.com/H3GvlpUBh1ZJ1KNCEteaGU
 
-*2.-* https://chat.whatsapp.com/Bi2Oql7vCjVJf2sFAZOzNi
+*2.-* https://chat.whatsapp.com/H3GvlpUBh1ZJ1KNCEteaGU
 
-*3.-* https://chat.whatsapp.com/Bi2Oql7vCjVJf2sFAZOzNi
+*3.-* https://chat.whatsapp.com/H3GvlpUBh1ZJ1KNCEteaGU
 
-*4.-* https://chat.whatsapp.com/Bi2Oql7vCjVJf2sFAZOzNi
+*4.-* https://chat.whatsapp.com/H3GvlpUBh1ZJ1KNCEteaGU
 
-*5.-* https://chat.whatsapp.com/Bi2Oql7vCjVJf2sFAZOzNi`.trim();
+*5.-* https://chat.whatsapp.com/H3GvlpUBh1ZJ1KNCEteaGU `.trim();
   const buttonMessage= {
     'document': {url: `https://github.com/MESIASREYES/TheMystic-Bot-MD`},
     'mimetype': `application/${document}`,
